7f60658ab8e8312d35a6659e893e2d3bdf347070
Dahald <contact@dahdev.pl>
2017-05-04 12:16:30 +0200

    Change license to MIT

--- src/main/java/pl/dahdev/bloomfilters/BloomFilter.java
+++ src/main/java/pl/dahdev/bloomfilters/BloomFilter.java
@@ -1,18 +1,26 @@
 /**
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * <p>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * <p>
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * MIT License
+ * <p/>
+ * Copyright (c) 2017 Damian Stygar
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
-
 package pl.dahdev.bloomfilters;
 
 import java.io.Serializable;
@@ -20,14 +28,13 @@ import java.util.Collection;
 
 /**
  * Interface of Bloom Filter.
- * <p>
+ * <p/>
  * A Bloom filter is a space-efficient probabilistic data structure
  * that is used to test whether an element is a member of a set.
  * A query returns either "possibly in set" or "definitely not in set".
  * Elements can be added to the set, but not removed.
  *
  * @param <E> - the type of elements in this Bloom Filter.
- *
  * @author Damian Stygar
  * @see <www.dahdev.pl>
  */
